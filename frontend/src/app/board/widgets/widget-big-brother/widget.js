1a4c47daa5da4d8162125a350709c47d9d6a2d48
Tommi Kaura-Aho <tommi.kaura-aho@protacon.com>
2015-05-07 15:50:26 +0300

    Added persons to loosi and styled person to button

--- frontend/src/app/board/widgets/widget-big-brother/widget.js
+++ frontend/src/app/board/widgets/widget-big-brother/widget.js
@@ -34,49 +34,43 @@
                  ]
                }*/
 
-              $scope.rivit = [
-                {loossit: [
-                  {nimi: 'sauna', koko: 2},
-                  {nimi: 'takaovi', 'koko': 1},
-                  {nimi: 'IT', 'koko': 1},
-                  {nimi: 'IT', 'koko': 1},
-                  {nimi: 'IT', 'koko': 1},
-                  {nimi: 'IT', 'koko': 1},
-                  {nimi: 'Vallu', 'koko': 1},
-                  {nimi: 'Janne', 'koko': 1},
-                  {nimi: 'Olli', 'koko': 1},
-                  {nimi: 'Cella', 'koko': 1}
-                ]},
-                {loossit: [
-                  {nimi: 'saunatupa', koko: 2},
-                  {nimi: 'tyhjä', 'koko': 1},
-                  {nimi: 'tyhjä', 'koko': 1},
-                  {nimi: 'tyhjä', 'koko': 1},
-                  {nimi: 'Keski 1', 'koko': 1},
-                  {nimi: 'Taukotila', 'koko': 2},
-                  {nimi: 'Keski 2', 'koko': 1},
-                  {nimi: 'tyhjä', 'koko': 1},
-                  {nimi: 'Atrium', 'koko': 1}
-                ]},
-                {loossit: [
-                  {nimi: 'huoneet', koko: 2},
-                  {nimi: 'myynti', 'koko': 1},
-                  {nimi: 'Ohjelmisto 5', 'koko': 1},
-                  {nimi: 'Ohjelmisto 4', 'koko': 1},
-                  {nimi: 'Ohjelmisto 3', 'koko': 1},
-                  {nimi: 'Ohjelmisto 2', 'koko': 1},
-                  {nimi: 'Ohjelmisto 1', 'koko': 1},
-                  {nimi: 'Toimistppalvelut', 'koko': 1},
-                  {nimi: 'Etuovi', 'koko': 1},
-                  {nimi: 'Villa', 'koko': 1}
-                ]}
+              $scope.loossit = [
+                  {name: 'sauna', width: 2, people: [
+                    {name: 'Testi 1', in: true},
+                    {name: 'Testi 2', in: false},
+                    {name: 'Testi 3', in: true}
+                  ]},
+                  {name: 'IT', 'width': 1},
+                  {name: 'IT', 'width': 1},
+                  {name: 'IT', 'width': 1},
+                  {name: 'IT', 'width': 1},
+                  {name: 'Vallu', 'width': 1,  people: [
+                    {name: 'Tarmo', in: true},
+                    {name: 'Matti', in: true},
+                    {name: 'Seppo', in: true}
+                  ]},
+                  {name: 'Janne', 'width': 1},
+                  {name: 'Olli', 'width': 1},
+                  {name: 'Cella', 'width': 1},
+                  {name: 'saunatupa', width: 2},
+                  {name: 'tyhjä', 'width': 1},
+                  {name: 'tyhjä', 'width': 1},
+                  {name: 'Keski 1', 'width': 1},
+                  {name: 'tyhjä', 'width': 1},
+                  {name: 'Taukotila', 'width': 2},
+                  {name: 'Keski 2', 'width': 1},
+                  {name: 'tyhjä', 'width': 1},
+                  {name: 'Atrium', 'width': 1},
+                  {name: 'huoneet', width: 2},
+                  {name: 'myynti', 'width': 1},
+                  {name: 'Ohjelmisto 5', 'width': 1},
+                  {name: 'Ohjelmisto 4', 'width': 1},
+                  {name: 'Ohjelmisto 3', 'width': 1},
+                  {name: 'Ohjelmisto 2', 'width': 1},
+                  {name: 'Ohjelmisto 1', 'width': 1},
+                  {name: 'Toimistopalvelut', 'width': 1},
+                  {name: 'Villa', 'width': 1}
               ];
-
-              $scope.loossit = {
-                'sauna': {
-                  'rivi': 1
-                }
-              };
             }
           ]
         };
