f439d292776d3d22b622b89a478b6afb920f5904
vilbertti <ville.pelho@gmail.com>
2015-05-07 16:04:40 +0300

    Twitter widget id settings form. Resizing.

--- frontend/src/app/board/board-services.js
+++ frontend/src/app/board/board-services.js
@@ -33,7 +33,14 @@
         },
         {
           name: 'Protacon twitter feed',
-          directive: 'widget-twitter'
+          directive: 'widget-twitter',
+          settingsModalOptions: {
+            partialTemplateUrl: '/frontend/board/widgets/widget-twitter/settings.html'
+          },
+          dataModelOptions: {
+            widget_id: '596256646311104512'
+          }
+
         }
       ];
     })
