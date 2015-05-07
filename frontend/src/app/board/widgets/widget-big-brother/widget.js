aad0b72c68c1855475b2f6b75ab1669a114200a2
Tommi Kaura-Aho <tommi.kaura-aho@protacon.com>
2015-05-07 14:38:24 +0300

    Loossi data as json and printing it to view

--- frontend/src/app/board/widgets/widget-big-brother/widget.js
+++ frontend/src/app/board/widgets/widget-big-brother/widget.js
@@ -18,23 +18,65 @@
             function controller($scope) {
               $scope.title = "Big Brother 2015";
 
-
-              $scope.loossit = [
-                {
+              /**
+               * {
                   name: "Testiloosi",
                   people: [
                     {name: 'Tommi', in: true},
                     {name: 'Timo', in: true}
                   ]
                 },
-                {
-                  name: "Testiloosi 2",
-                  people: [
-                    {name: 'Testi', in: false},
-                    {name: 'Antti', in: true}
-                  ]
-                }
+               {
+                 name: "Testiloosi 2",
+                 people: [
+                   {name: 'Testi', in: false},
+                   {name: 'Antti', in: true}
+                 ]
+               }*/
+
+              $scope.rivit = [
+                {loossit: [
+                  {nimi: 'sauna', koko: 2},
+                  {nimi: 'takaovi', 'koko': 1},
+                  {nimi: 'IT', 'koko': 1},
+                  {nimi: 'IT', 'koko': 1},
+                  {nimi: 'IT', 'koko': 1},
+                  {nimi: 'IT', 'koko': 1},
+                  {nimi: 'Vallu', 'koko': 1},
+                  {nimi: 'Janne', 'koko': 1},
+                  {nimi: 'Olli', 'koko': 1},
+                  {nimi: 'Cella', 'koko': 1}
+                ]},
+                {loossit: [
+                  {nimi: 'saunatupa', koko: 2},
+                  {nimi: 'tyhjä', 'koko': 1},
+                  {nimi: 'tyhjä', 'koko': 1},
+                  {nimi: 'tyhjä', 'koko': 1},
+                  {nimi: 'Keski 1', 'koko': 1},
+                  {nimi: 'Taukotila', 'koko': 2},
+                  {nimi: 'Keski 2', 'koko': 1},
+                  {nimi: 'tyhjä', 'koko': 1},
+                  {nimi: 'Atrium', 'koko': 1}
+                ]},
+                {loossit: [
+                  {nimi: 'huoneet', koko: 2},
+                  {nimi: 'myynti', 'koko': 1},
+                  {nimi: 'Ohjelmisto 5', 'koko': 1},
+                  {nimi: 'Ohjelmisto 4', 'koko': 1},
+                  {nimi: 'Ohjelmisto 3', 'koko': 1},
+                  {nimi: 'Ohjelmisto 2', 'koko': 1},
+                  {nimi: 'Ohjelmisto 1', 'koko': 1},
+                  {nimi: 'Toimistppalvelut', 'koko': 1},
+                  {nimi: 'Etuovi', 'koko': 1},
+                  {nimi: 'Villa', 'koko': 1}
+                ]}
               ];
+
+              $scope.loossit = {
+                'sauna': {
+                  'rivi': 1
+                }
+              };
             }
           ]
         };
