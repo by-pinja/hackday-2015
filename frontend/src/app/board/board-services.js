f78214d276d978509903040968d1da9fb0e3511a
Tarmo Leppänen <tarmo.leppanen@protacon.com>
2015-05-08 05:23:22 +0300

    Now widgets have titles!

--- frontend/src/app/board/board-services.js
+++ frontend/src/app/board/board-services.js
@@ -31,19 +31,25 @@
           name: 'Worms Ladder',
           directive: 'widget-worms-ladder',
           dataAttrName: 'players',
-          dataModelType: 'WidgetWormsLadderModel'
+          dataModelType: 'WidgetWormsLadderModel',
+          title: 'OG Worms ladders, yo',
+          showTitle: true
         },
         {
           name: 'Coffee Status',
           directive: 'widget-coffee-scale',
           dataAttrName: 'pots',
-          dataModelType: 'widgetCoffeeScaleModel'
+          dataModelType: 'widgetCoffeeScaleModel',
+          title: 'I would like to have some coffeez, plz',
+          showTitle: true
         },
         {
           name: 'Message of the Day',
           directive: 'widget-message-viewer',
           dataAttrName: 'messages',
-          dataModelType: 'widgetMessageViewerModel'
+          dataModelType: 'widgetMessageViewerModel',
+          title: 'I would like to have some coffeez, plz',
+          showTitle: true
         },
         {
           name: 'Protacon video',
@@ -63,7 +69,9 @@
           },
           dataModelOptions: {
             widget_id: '596256646311104512'
-          }
+          },
+          title: 'Protacon @ twitter',
+          showTitle: true
         },
         {
           name: 'LiukkoClock',
@@ -82,7 +90,9 @@
           },
           settingsModalOptions: {
             partialTemplateUrl: '/frontend/board/widgets/widget-rss-reader/settings.html'
-          }
+          },
+          title: 'RSS feedz',
+          showTitle: true
         },
         {
           name: 'Weather Information',
